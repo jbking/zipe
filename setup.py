e17be27b80cf3005fd9bea930c32cfb53505f8ef
MURAOKA Yusuke <yusuke@jbking.org>
2012-03-13 15:55:39 +0900

    add information

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@ version = '0.1'
 
 setup(name='zipe',
       version=version,
-      description="zip/unzip with converting file name encoding",
+      description="A zip utility for cross file name encoding powered by Python.",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Utilities',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='MURAOKA Yusuke',
       author_email='yusuke@jbking.org',
-      url='',
+      url='https://github.com/jbking/zipe',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
